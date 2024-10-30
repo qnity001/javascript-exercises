e24a68e81b2b22ee35a3f0458c124f92d5cc1f2d
qnity001 <aishwaryam.aarya@gmail.com>
2024-10-31 02:02:46 +0530

    Solve assigment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) 
+        return "ERROR";
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
