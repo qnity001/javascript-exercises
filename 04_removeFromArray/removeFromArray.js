891cedb85d3cb66dae5bc5652138990a94ecda61
qnity001 <aishwaryam.aarya@gmail.com>
2024-10-31 01:58:42 +0530

    Solve assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            let n = arr.indexOf(arguments[j]);
+            if (n != -1) {
+                arr.splice(n, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
