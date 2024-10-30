abc3781d913ccfa82bf8c39ab1da7eb75177662e
qnity001 <aishwaryam.aarya@gmail.com>
2024-10-31 01:54:50 +0530

    Solve assigment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rev = "";
+    for (let i = str.length - 1; i >= 0; i-- ) {
+        let c = str.charAt(i);
+        rev += c;
+    }
+    return rev;
 };
 
 // Do not edit below this line
